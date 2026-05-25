17cc1493c4e31910d95ccb64c530fd181de97be3
saudihic <aalgragre@gmail.com>
2026-05-25 08:30:37 +0300

    Delete assets directory

--- assets/js/site.js
+++
@@ -1,4 +0,0 @@
-const observer = new IntersectionObserver((entries)=>{
-  entries.forEach(entry=>{ if(entry.isIntersecting) entry.target.classList.add('is-visible') })
-},{threshold:.14});
-document.querySelectorAll('.reveal').forEach(el=>observer.observe(el));
